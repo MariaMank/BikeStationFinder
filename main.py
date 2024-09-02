ea3dd3430a90f36bb8bc6ef9097ced1bfaa1fc7f
MariaMank <92314221+MariaMank@users.noreply.github.com>
2024-09-02 18:42:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,13 +3,6 @@ from solver import BikeStationSolver
 if __name__ == '__main__':
     algorytmy = ['MCLP', 'LSCP', 'P-median', 'P-center', 'All']
     results_path = f"./dane/"
-    propozycje_path = results_path + 'propozycjeP.geojson'
-    istniejace_path = results_path + 'existing_stations.geojson'
-    siec = results_path + 'NetP.geojson'
-    demands = "./mclpF/UrsusDane/living_houses.geojson"
-
-
-
     solver = BikeStationSolver(500,
                                 3,
                                 800,
@@ -18,6 +11,10 @@ if __name__ == '__main__':
     solver.solve()
     solver.getResults(results_path)
     """
+    propozycje_path = results_path + 'propozycjeP.geojson'
+    istniejace_path = results_path + 'existing_stations.geojson'
+    siec = results_path + 'NetP.geojson'
+    demands = "./mclpF/UrsusDane/living_houses.geojson"
     solver = BikeStationSolver(500,
                                3,
                                200,
